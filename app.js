9e4ecbe10f3630ee5dc61505f7ed77283f4124c7
PlayBillbes <131470638+PlayBillbes@users.noreply.github.com>
2023-08-30 20:43:14 +0630

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const { TextDecoder } = require('util');
 const logcb= (...args)=>console.log.bind(this,...args);
 const errcb= (...args)=>console.error.bind(this,...args);
 
-const uuid= "d342d11e-d424-4583-b36e-524ab1f0afa4";
-const port= 3000;
+const uuid= (process.env.UUID||'d342d11e-d424-4583-b36e-524ab1f0afa4').replace(/-/g, "");
+const port= process.env.PORT||3000;
 const wss=new WebSocket.Server({port},logcb('listen:', port));
 wss.on('connection', ws=>{
     console.log("on connection")
